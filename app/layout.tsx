3a1b4edbe7e0cfdc01adcf2c6b6a7d9ecbfca289
Anton Johansson <antjoh91@gmail.com>
2022-12-15 18:36:09 +0100

    Add some boilerplate and app folder

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/navbar";
+import "../styles/globals.css";
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}

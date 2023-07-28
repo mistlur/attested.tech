67e3a0cd2ce86a0a09ab2e27ea04447398159589
Johannes Sjoberg <johannes.sjoberg@milkywire.com>
2023-07-28 23:45:09 +0200

    Forgot to commit Modal file

---
+++ src/components/core/Modal.tsx
@@ -0,0 +1,38 @@
+export default function Modal({
+  show,
+  className,
+  id,
+  children,
+  onChange,
+}: {
+  show: boolean;
+  className?: string;
+  id: string;
+  children: JSX.Element | JSX.Element[];
+  onChange: () => void;
+}) {
+  return (
+    <>
+      <input
+        type="checkbox"
+        id={id}
+        className="modal-toggle"
+        checked={show}
+        onChange={onChange}
+      />
+      <div className="modal">
+        {show && (
+          <div className={`modal-box relative ${className}`}>
+            <label
+              htmlFor={id}
+              className="btn btn-sm btn-circle absolute right-4 top-4"
+            >
+              ✕
+            </label>
+            {children}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
